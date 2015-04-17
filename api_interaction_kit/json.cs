ca7b34d0e83d905efca6318626a2d01572717005
Kyle Avery <Xirdie@gmail.com>
2015-04-17 11:54:09 -0700

    serialization

--- api_interaction_kit/json.cs
+++ api_interaction_kit/json.cs
@@ -1,11 +1,40 @@
 ﻿using System;
+using System.Runtime.Serialization;
+using System.IO;
 
 namespace api_interaction_kit
 {
-	public class json
+	[DataContract]
+	public class json : Object
 	{
-		public json ()
+		
+	}
+		
+	public class information : json
+	{
+		[DataMember]
+		protected string first_name;
+		[DataMember]
+		protected string last_name;
+		[DataMember]
+		protected string age;
+		[DataMember]
+		protected string height; //Inches
+		[DataMember]
+		protected string weight; //pounds
+		[DataMember]
+		protected string sex;
+	}
+
+	public static class json_functions
+	{
+		static public System.Runtime.Serialization.Json.DataContractJsonSerializer serializer (Type T)
 		{
+			return (new System.Runtime.Serialization.Json.DataContractJsonSerializer (T));
+		}
+		static public Object deserializer (System.Runtime.Serialization.Json.DataContractJsonSerializer s)
+		{
+			
 		}
 	}
 }
