b54030f42cb0ff52dc9a1fc9ea7cf70181bb14d1
faustino-lukolo <faustino.lukolo@email.wsu.edu>
2015-04-23 12:15:09 -0700

    Base HomeView working

--- WearableSensorUI/Fragments/HomeFragment.cs
+++ WearableSensorUI/Fragments/HomeFragment.cs
@@ -19,18 +19,43 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using Android.OS;
-using Android.Views;
+
+using Android.Content;
+using Android.App;
 using Android.Support.V4.App;
+using Android.Support.V7.Widget;
+using System.Collections.Generic;
+using Android.Widget;
+using Android.Views;
 
 namespace WearableSensorUI
 {
-    public class HomeFragment : Fragment
+    public class HomeFragment : Android.Support.V4.App.Fragment
     {
+        private RecyclerView mRecyclerView;
+        private RecyclerView.LayoutManager mLayoutManager;
+        private RecyclerView.Adapter mAdapter;
+        private List<WearableActivity> ActivitiesDetected;
+
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
             // Create your fragment here
-        }
+            ActivitiesDetected = new List<WearableActivity>();
+
+           // Generate 20 Activities
+            for (int i = 0; i < 15; i++)
+            {
+                var DetectedActivity = new WearableActivity();
+                DetectedActivity.Generate();
+
+
+                ActivitiesDetected.Add(DetectedActivity);
+
+            }
+        }        
+
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
@@ -40,9 +65,18 @@ namespace WearableSensorUI
             var ignore = base.OnCreateView(inflater, container, savedInstanceState);
             var view = inflater.Inflate(Resource.Layout.fragment_home_page, container, false);
 
+            mRecyclerView = view.FindViewById<RecyclerView>(Resource.Id.recyclerView);
+
+
+            //Create our layout manager
+            mLayoutManager = new LinearLayoutManager(view.Context);
+            mRecyclerView.SetLayoutManager(mLayoutManager);
+            mAdapter = new RecyclerAdapter(ActivitiesDetected, mRecyclerView, view.Context);
+            mRecyclerView.SetAdapter(mAdapter);
 
             return view;
         }
+
     }
 }
 
