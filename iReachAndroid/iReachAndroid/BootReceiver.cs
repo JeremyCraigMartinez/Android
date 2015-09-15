08e41cfb61972bf2cd3d15eb3b1adb0ecb89566a
Kyle Avery <Xirdie@gmail.com>
2015-09-15 12:08:33 -0700

    Boot on launch

---
+++ iReachAndroid/iReachAndroid/BootReceiver.cs
@@ -0,0 +1,17 @@
+﻿using Android;
+using Android.Content;
+
+//Reference: http://javatechig.com/android/how-to-start-an-application-at-device-bootup-in-android
+
+namespace iReachAndroid.Activities
+{
+	public class BootReceiver : BroadcastReceiver
+	{
+		public override void OnReceive (Context context, Intent intent)
+		{
+			Intent i = new Intent (context, typeof(MainActivity));  
+			i.AddFlags (ActivityFlags.NewTask);
+			context.StartActivity (i);  
+		}
+	}
+}
