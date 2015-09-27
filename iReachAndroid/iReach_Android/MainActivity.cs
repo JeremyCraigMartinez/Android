30794a03b609f02cb44ce9e49d55b035d5b39ab3
Kyle Avery <Xirdie@gmail.com>
2015-09-26 17:40:21 -0700

    jitter base

---
+++ iReachAndroid/iReach_Android/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace iReach_Android
+{
+	[Activity (Label = "iReach_Android", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			RequestWindowFeature(WindowFeatures.NoTitle);
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button login_button = FindViewById<Button> (Resource.Id.login_button);
+			
+			login_button.Click += delegate {
+				
+			};
+		}
+	}
+}
+
+
